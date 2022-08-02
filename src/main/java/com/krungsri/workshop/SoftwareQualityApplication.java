debf4d80a91fa2b9d2b244685ca2d3f1a80cac1d
worasit <worasit.daimongkol@thoughtworks.com>
2022-08-02 11:48:26 +0700

    add missing files

---
+++ src/main/java/com/krungsri/workshop/SoftwareQualityApplication.java
@@ -0,0 +1,13 @@
+package com.krungsri.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareQualityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareQualityApplication.class, args);
+	}
+
+}
