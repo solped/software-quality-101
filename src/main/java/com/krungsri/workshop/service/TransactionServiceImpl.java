9a6e4eab67e66e6814c149133804122d647c496b
worasit <worasit.dmk501@gmail.com>
2022-08-02 18:36:49 +0700

    Added mockup

--- src/main/java/com/krungsri/workshop/service/TransactionServiceImpl.java
+++ src/main/java/com/krungsri/workshop/service/TransactionServiceImpl.java
@@ -1,12 +1,13 @@
 package com.krungsri.workshop.service;
 
 import com.krungsri.workshop.model.Transaction;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.List;
 
 public class TransactionServiceImpl implements TransactionService {
     @Override
     public void processTransaction(List<Transaction> transactions) {
-
+        throw new NotImplementedException();
     }
 }
