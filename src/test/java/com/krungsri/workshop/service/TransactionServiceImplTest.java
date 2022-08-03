e647f4e6a843f3cc35f178512f2998da3452c75f
worasit <worasit.dmk501@gmail.com>
2022-08-03 13:26:01 +0700

    Completed the unit test for workshop2

--- src/test/java/com/krungsri/workshop/service/TransactionServiceImplTest.java
+++ src/test/java/com/krungsri/workshop/service/TransactionServiceImplTest.java
@@ -10,6 +10,7 @@ import com.krungsri.workshop.model.PaymentMethod;
 import com.krungsri.workshop.model.PaymentType;
 import com.krungsri.workshop.model.Transaction;
 import com.krungsri.workshop.model.TransactionStatus;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -55,5 +56,78 @@ class TransactionServiceImplTest {
         // then
         verify(paypalPaymentProvider, times(1)).processPayment(paypalPaymentTransaction);
         verify(paypalPaymentProvider, times(1)).processPayment(paypalRefundTransaction);
+    }
+
+    @Test
+    void givenEmptyTransactions_whenProcessTransaction_thenThrowNoTransactionProvided() throws NoTransactionProvided, InvalidTransactionPaymentType, InvalidTransactionPaymentMethod {
+        // given
+        ArrayList<Transaction> emptyTransactions = new ArrayList<>();
+
+        CreditCardPaymentProvider creditCardPaymentProvider = spy(new CreditCardPaymentProvider());
+        PaypalPaymentProvider paypalPaymentProvider = spy(new PaypalPaymentProvider());
+        CryptoPaymentProvider cryptoPaymentProvider = spy(new CryptoPaymentProvider());
+        TransactionServiceImpl transactionService = new TransactionServiceImpl(
+                creditCardPaymentProvider,
+                paypalPaymentProvider,
+                cryptoPaymentProvider
+        );
+
+        // when & then
+        Assertions.assertThrows(NoTransactionProvided.class,
+                () -> transactionService.processTransaction(emptyTransactions));
+    }
+
+    @Test
+    void givenInvalidPaymentTypeTransactions_whenProcessTransaction_thenThrowInvalidTransactionPaymentType() throws NoTransactionProvided, InvalidTransactionPaymentType, InvalidTransactionPaymentMethod {
+        // given
+        ArrayList<Transaction> transactions = new ArrayList<>();
+        Transaction paypalRefundTransaction = Transaction.builder()
+                .id(1)
+                .status(TransactionStatus.OPEN)
+                .type(PaymentType.INVALID)
+                .method(PaymentMethod.PAYPAL)
+                .amount(200.00)
+                .build();
+        transactions.add(paypalRefundTransaction);
+
+        CreditCardPaymentProvider creditCardPaymentProvider = spy(new CreditCardPaymentProvider());
+        PaypalPaymentProvider paypalPaymentProvider = spy(new PaypalPaymentProvider());
+        CryptoPaymentProvider cryptoPaymentProvider = spy(new CryptoPaymentProvider());
+        TransactionServiceImpl transactionService = new TransactionServiceImpl(
+                creditCardPaymentProvider,
+                paypalPaymentProvider,
+                cryptoPaymentProvider
+        );
+
+        // when & then
+        Assertions.assertThrows(InvalidTransactionPaymentType.class,
+                () -> transactionService.processTransaction(transactions));
+    }
+
+    @Test
+    void givenInvalidPaymentMethodTransactions_whenProcessTransaction_thenThrowInvalidTransactionPaymentMethod() throws NoTransactionProvided, InvalidTransactionPaymentType, InvalidTransactionPaymentMethod {
+        // given
+        ArrayList<Transaction> transactions = new ArrayList<>();
+        Transaction paypalRefundTransaction = Transaction.builder()
+                .id(1)
+                .status(TransactionStatus.OPEN)
+                .type(PaymentType.PAYMENT)
+                .method(PaymentMethod.NOT_SUPPORT)
+                .amount(200.00)
+                .build();
+        transactions.add(paypalRefundTransaction);
+
+        CreditCardPaymentProvider creditCardPaymentProvider = spy(new CreditCardPaymentProvider());
+        PaypalPaymentProvider paypalPaymentProvider = spy(new PaypalPaymentProvider());
+        CryptoPaymentProvider cryptoPaymentProvider = spy(new CryptoPaymentProvider());
+        TransactionServiceImpl transactionService = new TransactionServiceImpl(
+                creditCardPaymentProvider,
+                paypalPaymentProvider,
+                cryptoPaymentProvider
+        );
+
+        // when & then
+        Assertions.assertThrows(InvalidTransactionPaymentMethod.class,
+                () -> transactionService.processTransaction(transactions));
     }
 }
